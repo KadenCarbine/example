7bd760f286e7996849ada31d489346991f87adb6
Kaden Carbine <kadenjcar@gmail.com>
2025-04-10 16:34:46 -0400

    Episode 24

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,22 @@ use App\Http\Controllers\SessionController;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
-Route::resource('jobs', JobController::class);
+
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+    ->middleware('auth')
+    ->can('edit', 'job');
+
+Route::patch('/jobs/{job}', [JobController::class, 'update'])
+    ->middleware('auth')
+    ->can('edit', 'job');
+Route::delete('/jobs/{job}', [JobController::class, 'destroy'])
+    ->middleware('auth')
+    ->can('edit', 'job');
 
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
