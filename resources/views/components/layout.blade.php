e9f5f7d9f8e89138c670f5838b4d8bd60e836c23
Kaden Carbine <kadenjcar@gmail.com>
2025-04-13 21:17:12 -0400

    Episode 26

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        <script src="https://cdn.tailwindcss.com"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body  class="h-full">
 <div class="min-h-full">
